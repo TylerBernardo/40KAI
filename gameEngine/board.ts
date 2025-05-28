2bc42a31a16b8ee9b0df137166ec7b73db321ba4
Tyler <tylerjbernardo@gmail.com>
2025-05-28 16:16:34 -0700

    Code at least compiles, still crashes

--- gameEngine/board.ts
+++ gameEngine/board.ts
@@ -1,50 +1,5 @@
 import {UnitWrapper} from "./units.ts"
-
-//represents a single tile in the board. holds information about that tile. Each tile represents a 1 inch x 1 inch square on a in person board
-class Tile{
-    x: number = -1;
-    y: number = -1;
-    blocksLOS:boolean = false;
-    boardObjects:BoardObject[] = Array();
-    constructor(x:number,y:number){
-        this.x = x;
-        this.y = y;
-    }
-    //remove the BoardObject that has the name "objectName"
-    //returns true if something was removed, returns false if nothing was found
-    removeObject(objectName:string): boolean{
-        for(var i = 0; i < this.boardObjects.length;i++){
-            if(this.boardObjects[i].name == objectName){
-                this.boardObjects.splice(i,1);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    toString(): string{
-        let output: string = "";
-        //console.log(this.boardObjects)
-        for(let object of this.boardObjects){
-            output += object.name;
-            if(object instanceof UnitWrapper){
-                let castedUnit: UnitWrapper = object;
-                //TODO: print more useful info here
-                output += "(" + castedUnit.name + ")"
-            }
-        }
-        return output;
-    }
-    
-        /*
-        //target tile should always be greater than the 
-        if(targetTile.x < this.x || targetTile.y < this.y){
-            return targetTile.lineOfSight(this);
-        }
-            */
-
-};
-
+import {Tile, BoardObject, Terrain} from "./boardObject.ts"
 //represents the game board. Keeps track of unit positions, tiles on the board, and other things.
 class Board{
     height:number = 0;
@@ -132,50 +87,11 @@ class Board{
 };
 
 //represents a object on the board
-class BoardObject{
-    name: string = "";
-    currentTile: Tile;
-    constructor(tile:Tile,name:string){
-        this.name = name;
-        this.currentTile = tile;
-        tile.boardObjects.push(this);
-    }
-    //move this object to the given tile
-    move(dTile:Tile): void{
-        dTile.boardObjects.push(this);
-        if(this.currentTile != null){
-            this.currentTile.removeObject(this.name);
-        }
-        this.currentTile = dTile;
-    }
-    //remove this object from the board
-    remove(): void{
-        if(this.currentTile != null){
-            this.currentTile.removeObject(this.name);
-        }
-        //delete this;
-    }
 
-    getType():string{return "BoardObject"}
-}
-
-//can block line of sight, block movement, or both
-class Terrain extends BoardObject{
-    blocksMovement:boolean = false;
-    blocksLOS:boolean = false;
-    constructor(tile:Tile,name:string,blocksMovement:boolean,blocksLOS:boolean){
-        super(tile,name);
-        this.blocksMovement = blocksMovement;
-        this.blocksLOS = blocksLOS;
-        tile.blocksLOS = blocksLOS;
-    }
-
-    getType():string{return "Terrain"}
-}
 
 export {
-    Tile,
     Board,
-    BoardObject,
-    Terrain
+    Terrain,
+    Tile,
+    BoardObject
 }
---
+++ gameEngine/boardObject.ts
@@ -0,0 +1,73 @@
+//represents a single tile in the board. holds information about that tile. Each tile represents a 1 inch x 1 inch square on a in person board
+class Tile{
+    x: number = -1;
+    y: number = -1;
+    blocksLOS:boolean = false;
+    boardObjects:BoardObject[] = Array();
+    constructor(x:number,y:number){
+        this.x = x;
+        this.y = y;
+    }
+    //remove the BoardObject that has the name "objectName"
+    //returns true if something was removed, returns false if nothing was found
+    removeObject(objectName:string): boolean{
+        for(var i = 0; i < this.boardObjects.length;i++){
+            if(this.boardObjects[i].name == objectName){
+                this.boardObjects.splice(i,1);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+        /*
+        //target tile should always be greater than the 
+        if(targetTile.x < this.x || targetTile.y < this.y){
+            return targetTile.lineOfSight(this);
+        }
+            */
+
+};
+
+class BoardObject{
+    name: string = "";
+    currentTile: Tile;
+    constructor(tile:Tile,name:string){
+        this.name = name;
+        this.currentTile = tile;
+        tile.boardObjects.push(this);
+    }
+    //move this object to the given tile
+    move(dTile:Tile): void{
+        dTile.boardObjects.push(this);
+        if(this.currentTile != null){
+            this.currentTile.removeObject(this.name);
+        }
+        this.currentTile = dTile;
+    }
+    //remove this object from the board
+    remove(): void{
+        if(this.currentTile != null){
+            this.currentTile.removeObject(this.name);
+        }
+        //delete this;
+    }
+
+    getType():string{return "BoardObject"}
+}
+
+//can block line of sight, block movement, or both
+class Terrain extends BoardObject{
+    blocksMovement:boolean = false;
+    blocksLOS:boolean = false;
+    constructor(tile:Tile,name:string,blocksMovement:boolean,blocksLOS:boolean){
+        super(tile,name);
+        this.blocksMovement = blocksMovement;
+        this.blocksLOS = blocksLOS;
+        tile.blocksLOS = blocksLOS;
+    }
+
+    getType():string{return "Terrain"}
+}
+
+export {Tile,BoardObject,Terrain}
